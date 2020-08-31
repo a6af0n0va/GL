45efe307a4dee92326279f9951ec7bbef2e099f2
a6af0n0va <61389484+a6af0n0va@users.noreply.github.com>
2020-08-31 21:58:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QApplication, QOpenGLWidget
+
+class OGLQUAD(QOpenGLWidget):
+	def initializeGL(self):
+		pass
+	def resizeGL(self, width, height):
+		pass
+	def paintGL(self):
+		pass
+
+if __name__=="__main__":
+	app = QApplication([])
+	
+	glquad = OGLQUAD()
+	glquad.resize(200, 200)
+	glquad.show()
+	
+	app.exec()
+	
