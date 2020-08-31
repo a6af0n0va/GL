929325008a27e8155474c0c0cea5b55811700123
a6af0n0va <lizaagafonova2019@mail.ru>
2020-08-31 22:27:34 +0300

    la

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from PyQt5.QtWidgets import QApplication, QOpenGLWidget
-#ЭТОКВАДРАТИГ
+from PyQt5.QtGui import QOpenGLContext
+import OpenGL.GL as gl
 class OGLQUAD(QOpenGLWidget):
+	def __init__(self):
+		super().__init__()
+		
 	def initializeGL(self):
-		pass
+		print("init")
+		#pfunc = QOpenGLContext.currentContext().functions()
+		gl.glClearColor(0,0,0,1)
 	def resizeGL(self, width, height):
-		pass
+		print("resiz")
+		gl.glMatrixMode(GL_PROJECTION)
+		gl.glLoadIdentity()
+		gl.glViewport(0,0, width, height)
+		gl.glOrtho(0,100,100,0,-1,1)
 	def paintGL(self):
-		pass
-
+		print("paint")
+		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		gl.glBegin(GL_QUADS)
+		gl.glColor3f(1,0,0)
+		gl.glVertex2f(0,100)
+		gl.glColor3f(0,1,0)
+		gl.glVertex2f(100,100)		
+		gl.glColor3f(0,0,1)
+		gl.glVertex2f(100,0)		
+		gl.glColor3f(1,1,1)
+		gl.glVertex2f(0,0)
+		gl.glEnd()
 if __name__=="__main__":
 	app = QApplication([])
 	
